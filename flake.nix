373bacd5c31e21e5c96c52883964f71509176a91
emibyte <fevymarine@gmail.com>
2025-09-25 22:03:48 +0200

    project setup

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "dev flake for flowery press";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3Packages.python-lsp-server
+
+            # LSP server plugins of interest:
+            python3Packages.pylsp-mypy
+            python3Packages.pylsp-rope
+            python3Packages.python-lsp-ruff
+          ];
+        };
+      });
+    };
+}
