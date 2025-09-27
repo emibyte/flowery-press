61449fbd4d774e98c96cf755ac6d8057c36a8309
emibyte <fevymarine@gmail.com>
2025-09-27 20:41:14 +0200

    forgot a quote

--- src/textnode.py
+++ src/textnode.py
@@ -43,4 +43,4 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
         case TextType.IMAGE:
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
-            raise ValueError(f"text type '{text_node.text_type} not supported")
+            raise ValueError(f"text type '{text_node.text_type}' not supported")
