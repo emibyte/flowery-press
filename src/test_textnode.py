dc49d69ab8c7d1468d08cc2a90adb7cf0db436d8
emibyte <fevymarine@gmail.com>
2025-09-27 20:18:33 +0200

    textnode to htmlnode conversion + tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -32,6 +32,27 @@ class TestTextNode(unittest.TestCase):
     def test_url_none(self):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node.url, None)
+
+    def test_conversion_to_html_node_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_conversion_to_html_node_image(self):
+        node = TextNode("Alternative text", TextType.IMAGE, "./images/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props, {"src": "./images/image.png", "alt": "Alternative text"}
+        )
+
+    def test_conversion_to_html_node_italic(self):
+        node = TextNode("This is some italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is some italic text")
 
 
 if __name__ == "__main__":
