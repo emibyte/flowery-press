47028f1f1f2177a07435aea00e1515db0c7d93be
emibyte <fevymarine@gmail.com>
2025-09-30 20:34:30 +0200

    allow building with custom base path for deployment

--- src/generate_page.py
+++ src/generate_page.py
@@ -11,29 +11,39 @@ def extract_title(markdown: str) -> str:
     raise Exception("no h1 header found in markdown")
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(
+    from_path: str, template_path: str, dest_path: str, base_path: str = "/"
+) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_contents = ""
-    template = ""
+    final_html = ""
     with open(from_path, mode="r") as f:
         markdown_contents = f.read()
     with open(template_path, mode="r") as f:
-        template = f.read()
+        final_html = f.read()
 
     html = markdown_to_html_node(markdown_contents).to_html()
     title = extract_title(markdown_contents)
 
-    template = template.replace("{{ Content }}", html).replace("{{ Title }}", title)
+    final_html = (
+        final_html.replace("{{ Content }}", html)
+        .replace("{{ Title }}", title)
+        .replace('href="/', f'href="{base_path}')
+        .replace('src="/', f'src="{base_path}')
+    )
 
     directory = os.path.dirname(dest_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     with open(dest_path, mode="w") as f:
-        f.write(template)
+        f.write(final_html)
     return
 
-def generate_pages_recursively(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+
+def generate_pages_recursively(
+    dir_path_content: str, template_path: str, dest_dir_path: str, base_path: str = "/"
+) -> None:
     for file in os.listdir(dir_path_content):
         source_file_path = os.path.join(dir_path_content, file)
         dest_file_path = os.path.join(dest_dir_path, file)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import sys
 from dump_files import move_and_update_all_files
 from generate_page import generate_pages_recursively
 
 
 def main():
-    move_and_update_all_files("static", "public")
+    dir_to_build = "docs"
+    move_and_update_all_files("static", dir_to_build)
     # generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages_recursively("content", "template.html", "public")
+    if len(sys.argv) >= 2:
+        base_path = sys.argv[1]
+        generate_pages_recursively("content", "template.html", dir_to_build, base_path)
+    else:
+        generate_pages_recursively("content", "template.html", dir_to_build)
 
 
 main()
